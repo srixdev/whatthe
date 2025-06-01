eb4bf2a8495609d534fedb7682fd990617bc1a62
Simik <simonsandala10@gmail.com>
2025-06-01 17:40:23 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,43 +1,61 @@
 const express = require('express');
 const mongoose = require('mongoose');
-
 const path = require('path');
-const configRouter = require('./route/config');
 
+const configRouter = require('./route/config');
 const userRouter = require('./route/user');
 const gamesRouter = require('./route/games');
-
 const decorationRouter = require('./route/decoration');
 const mailboxRouter = require('./route/mailbox');
-
 const databaseConfig = require('./config/database');
 
-// Dont change these
 const app = express();
 const port = 3000;
 
 mongoose.connect(databaseConfig.databaseUrl)
   .then(() => {
-    console.log(`The connection to database ${databaseConfig.databaseType} is made and working`);
+    console.log(`You are connected to ${databaseConfig.databaseType} `);
   })
   .catch((error) => {
-    console.log('Error connecting to MongoDB Atlas:', error);
-});
+    console.log('You have been not connected to MongoDb. Reason:', error);
+  });
 
 app.use(express.json());
 
 app.use('/config', configRouter);
 app.use('/user', userRouter);
-app.use('/game', gamesRouter);
-
+app.use('/games', gamesRouter);
 app.use('/decoration', decorationRouter);
-
 app.use('/mailbox', mailboxRouter);
 
 app.all('/', (req, res) => {
-  res.status(200).send('Hello World');
+  res.status(200).send('blockman planet works');
 });
 
 app.listen(port, () => {
-  console.log(`Success your server is available at http://localhost:${port}`);
+  const colors = ['\x1b[31m', '\x1b[33m', '\x1b[32m', '\x1b[36m', '\x1b[34m', '\x1b[35m', '\x1b[37m'];
+const reset = '\x1b[0m';  // Reset color to default
+
+// First message
+const message1 = `Connected at localhost:${port}`;
+let coloredMessage1 = '';
+
+for (let i = 0; i < message1.length; i++) {
+  coloredMessage1 += colors[i % colors.length] + message1[i];
+}
+
+console.log(coloredMessage1 + reset);
+
+// Second message
+const message2 = 'Dispatch have been Connected Successful!';
+let coloredMessage2 = '';
+
+for (let i = 0; i < message2.length; i++) {
+  coloredMessage2 += colors[i % colors.length] + message2[i];
+}
+
+console.log(coloredMessage2 + reset);
+
+  console.log("\x1b[31m All Files have been Successfuly Loaded!!");
+  console.log("\x1b[31mAll credits to BlockyMod the official owner Discord: blockymod\x1b[0m");
 });
